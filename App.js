294f639333672ad5320b1e1d4ab53d6d6adbd9d6
Andres Urdaneta <aeum3893@gmail.com>
2019-12-24 15:28:29 -0500

    added style to the header, background color, centered text with flex, padding top

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.screen}>
-
+      <Header title="Guess the Number" />
     </View>
   );
 }
