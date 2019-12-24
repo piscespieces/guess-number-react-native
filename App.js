f0bc20f346666a6489b90f6de5dd477b77967296
Andres Urdaneta <aeum3893@gmail.com>
2019-12-24 15:16:58 -0500

    cleaned app.js, and created the components folder with the Header component file

--- App.js
+++ App.js
@@ -3,17 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
