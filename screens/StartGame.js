dc3199539f7ba5673c905c0ef6d2a6bee31700aa
Andres Urdaneta <aeum3893@gmail.com>
2019-12-26 17:13:19 -0500

    centered placeholder of the input field

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+
+import AwesomeButtonRick from 'react-native-really-awesome-button/src/themes/rick';
 
 const StartGame = props => {
     return (
         <View style={styles.screen}>
-            <Text>
-                The Game Screen
-            </Text>
-        </View>
+            <Text style={styles.headerText}>Start a New Game</Text>
+            <View style={styles.inputFormContainer}>
+                <TextInput
+                    placeholder="Select a Number"
+                    style={styles.textInput}
+                />
+                <View style={styles.buttonContainer}>
+                    <AwesomeButtonRick
+                        title="Reset"
+                        onPress={() => { }}
+                        type="secondary"
+                        style={styles.resetButton}
+                    >
+                        Reset
+                    </AwesomeButtonRick>
+
+                    <AwesomeButtonRick
+                        title="Confirm"
+                        onPress={() => { }}
+                        type="primary"
+                        style={styles.confirmButton}
+                    >
+                        Confirm
+                    </AwesomeButtonRick>
+                </View>
+            </View>
+        </View >
     )
 }
 
@@ -18,5 +43,26 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    headerText: {
+        fontSize: 20,
+    },
+    inputFormContainer: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    textInput: {
+        width: '80%',
+        height: 30,
+        marginVertical: 30,
+        fontSize: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#E0E0E0',
+        textAlign: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-evenly',
     },
 })
